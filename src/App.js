8f4de9b59de65fcaef64ef4a160f8d32407de274
vanbot6 <vberg6@gmail.com>
2024-06-10 14:55:13 -0400

    3pm push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [recipeFormShown, showRecipeForm] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+
+  let submitRecipe = (event) => {
+    event.preventDefault();
+    let newRecipeName = document.getElementById('newRecipeName').value;
+    let newRecipeInstructions = document.getElementById('newRecipeInstructions').value;
+    setRecipes([...recipes, { name: newRecipeName, instructions: newRecipeInstructions }]);
+    showRecipeForm(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">My Recipes</h1>
+      {recipes.length === 0 ? <p>There are no recipes to list.</p> : (
+        <ul>
+          {recipes.map((recipe, index) => <li key={index}>{recipe.name}</li>)}
+        </ul>
+      )}
+      {
+        recipeFormShown ?
+          <form id="recipe-form" name="recipe-form" onSubmit={submitRecipe}>
+            <label htmlFor="newRecipeName">Recipe name: </label>
+            <input type="text" id="newRecipeName" />
+            <label htmlFor="newRecipeInstructions">Instructions:</label>
+            <textarea id="newRecipeInstructions" placeholder="write recipe instructions here..." />
+            <input type="submit" value="Submit" />
+          </form>
+          :
+          <button onClick={() => showRecipeForm(true)}>Add Recipe</button>
+      }
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,69 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('As a Chef, I want to store my recipes so that I can keep track of them.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  let recipeHeader = screen.getByText('My Recipes');
+  expect(recipeHeader).toBeInTheDocument();
+
+  let recipeList = screen.getByText('There are no recipes to list.');
+  expect(recipeList).toBeInTheDocument();
+  expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4);
+});
+
+test("contains an add recipe button", () => {
+  render(<App />);
+
+  let recipeHeader = screen.getByText('My Recipes');
+  let button = screen.getByRole('button', { name: 'Add Recipe' });
+  
+  expect(button).toBeInTheDocument();
+  expect(recipeHeader.compareDocumentPosition(button)).toBe(4);
+});
+
+test("contains an add recipe button that when clicked opens a form", async () => {
+  render(<App />);
+
+  let button = screen.getByRole('button', { name: 'Add Recipe' });
+  userEvent.click(button);
+
+  let form = await screen.findByRole('form');
+  expect(form).toBeInTheDocument();
+
+  // Match the labels exactly or use regex for flexibility
+  expect(screen.getByLabelText(/Recipe name/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Instructions/i)).toBeInTheDocument();
+
+  button = screen.queryByRole('button', { name: 'Add Recipe' });
+  expect(button).toBeNull();
+});
+
+test("shows new recipe after adding", async () => {
+  render(<App />);
+
+  // Add recipe
+  let button = screen.getByRole('button', {name: 'Add Recipe'});
+  userEvent.click(button);
+
+  // wait for the form/textbox to appear, used findBy because it returns a promise
+  let recipeNameBox = await screen.findByLabelText(/Recipe name/i);
+  let recipeInstructionBox = screen.getByLabelText(/Instructions/i);
+
+  // add recipe
+  const recipeName = 'Tofu Scramble Tacos';
+  const recipeInstructions = "1. heat a skillet on medium with a dollop of coconut oil\n2. warm flour tortillas";
+  userEvent.type(recipeNameBox, recipeName);
+  userEvent.type(recipeInstructionBox, recipeInstructions);
+
+  // click the submit button
+  let submitButton = screen.getByRole('button', {name: 'Submit'});
+  userEvent.click(submitButton);
+
+  // wait for text to appear, a timeout means it was never found
+  let recipe = await screen.findByText(recipeName);
+  expect(recipe).toBeInTheDocument();
 });
